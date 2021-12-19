430f2ba39a2e3c5d2372be6095a430b7cdbd6ef3
Jakub Smejkal <smejkal.jakub99@gmail.com>
2021-12-19 19:20:29 +0100

    final set up

--- randomValues.sh
+++ randomValues.sh
@@ -25,12 +25,12 @@ do
     echo "succ_prob = $succ_prob"
     echo "hold_prob = $hold_prob"
 
-    sed -i "s/            player_areas_weight = .*/            player_areas_weight = $v/" dicewars/ai/xgrunw00.py
-    sed -i "s/            biggest_region_weight = .*/            biggest_region_weight = $w/" dicewars/ai/xgrunw00.py
-    sed -i "s/            weak_borders_weight = .*/            weak_borders_weight = $x/" dicewars/ai/xgrunw00.py
-    sed -i "s/            weak_enemies_weight = .*/            weak_enemies_weight = $y/" dicewars/ai/xgrunw00.py
-
-    sed -i "s/if(hold_prob >= 0\..* and succ_prob >= 0\..*):/if(hold_prob >= $hold_prob and succ_prob >= $succ_prob):/" dicewars/ai/xgrunw00.py
+    sed -i "s/            player_areas_weight = .*/            player_areas_weight = $v/" dicewars/ai/xgrunw00/xgrunw00.py
+    sed -i "s/            biggest_region_weight = .*/            biggest_region_weight = $w/" dicewars/ai/xgrunw00/xgrunw00.py
+    sed -i "s/            weak_borders_weight = .*/            weak_borders_weight = $x/" dicewars/ai/xgrunw00/xgrunw00.py
+    sed -i "s/            weak_enemies_weight = .*/            weak_enemies_weight = $y/" dicewars/ai/xgrunw00/xgrunw00.py
+    
+    sed -i "s/if(hold_prob >= 0\..* and succ_prob >= 0\..*):/if(hold_prob >= $hold_prob and succ_prob >= $succ_prob):/" dicewars/ai/xgrunw00/xgrunw00.py
 
     result=$(python3 ./scripts/dicewars-tournament.py -g 4 -n 100 -b 101 -s 1337)
     tmp=${result#*xgrunw00}
